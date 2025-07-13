b21b3eab947a8cd089ee5cb5c64ed76c224d8813
Adriely Floriano <56102850+Adrielycfloriano@users.noreply.github.com>
2025-07-13 17:23:41 -0300

    feat: Primeiro commit.

---
+++ src/main/java/com/thebookstore/TheBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.thebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBookstoreApplication.class, args);
+	}
+
+}
